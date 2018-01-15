c5e5fc993d040ba038833d89ef6b165fcd74087f
CL <277825228@qq.com>
2018-01-15 15:10:24 +0800

    Initial commit

---
+++ src/main/java/com/nuvole/framework/NuvoleFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.nuvole.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvoleFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuvoleFrameworkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nuvole/framework/NuvoleFrameworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nuvole.framework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NuvoleFrameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
